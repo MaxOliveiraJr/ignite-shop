483c706a435a7078618dd4ac4a7e8c420c390a42
Max Oliveira <max-oliveira@outlook.com.br>
2025-05-28 14:34:52 -0300

    stitches config initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { styled } from "../styles";
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  color: '$white',
+  border: 0,
+  padding: '4px 8px',
+  borderRadius: 4,
+
+  span: {
+    color: '$green300',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.9)'
+  }
+})
+
 export default function Home() {
-  return <div>Oi</div>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Button>
+        <span>Send</span>
+      </Button>
+    </>
+  )
 }
+
+
